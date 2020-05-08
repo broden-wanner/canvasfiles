cb38b1c839ae60dd171ac55b5be9c3d7c5d05b32
Broden Wanner <me@brodenwanner.com>
2020-05-08 00:25:59 -0500

    Created successful api call

--- public/content.js
+++ public/content.js
@@ -1,3 +1,19 @@
 'use strict';
 
 console.log('Extension works');
+
+const request = new Request('https://canvas.umn.edu/api/v1/courses', {
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+});
+
+fetch('https://canvas.umn.edu/api/v1/courses', {
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+})
+  .then((response) => response.json())
+  .then((classes) => console.log(classes));
