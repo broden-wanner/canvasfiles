78368b7e880c514d05c71ba1bfc35125baeda491
Broden Wanner <me@brodenwanner.com>
2020-05-08 10:57:01 -0500

    Added appbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import MainSection from './MainSection';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
+  const onClose = () => {
+    console.log('close');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Canvas Files
+          </Typography>
+          <IconButton
+            edge="end"
+            onClick={onClose}
+            className={classes.closeButton}
+            color="inherit"
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
       <MainSection />
     </div>
   );
