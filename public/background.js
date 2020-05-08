dd0e1481198365cc52410acf88c98fa63e04e874
Broden Wanner <me@brodenwanner.com>
2020-05-08 01:49:14 -0500

    Fixed message api

--- public/background.js
+++ public/background.js
@@ -14,3 +14,12 @@ chrome.runtime.onInstalled.addListener(() => {
     ]);
   });
 });
+
+// Called when the user clicks on the browser action
+chrome.browserAction.onClicked.addListener((tab) => {
+  // Send a message to the active tab
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, { message: 'toggle' });
+  });
+});
--- public/content.js
+++ public/content.js
@@ -1,18 +1,16 @@
 'use strict';
 
-function createSidePanel() {
-  const sidepanel = document.createElement('div');
-  sidepanel.style.background = 'white';
-  sidepanel.style.height = '100%';
-  sidepanel.style.width = '400px';
-  sidepanel.style.position = 'fixed';
-  sidepanel.style.top = '0px';
-  sidepanel.style.right = '0px';
-  sidepanel.style.zIndex = '9000000000000000000';
-  sidepanel.setAttribute('id', 'root');
-
-  document.body.appendChild(sidepanel);
-}
+// Create the sidepanel
+const sidepanel = document.createElement('div');
+sidepanel.style.background = 'white';
+sidepanel.style.height = '100%';
+sidepanel.style.width = '0px';
+sidepanel.style.position = 'fixed';
+sidepanel.style.top = '0px';
+sidepanel.style.right = '0px';
+sidepanel.style.zIndex = '9000000000000000000';
+sidepanel.setAttribute('id', 'root');
+document.body.appendChild(sidepanel);
 
 function toggle() {
   if (sidepanel.style.width == '0px') {
@@ -22,12 +20,9 @@ function toggle() {
   }
 }
 
-// Initialize the side panel
-createSidePanel();
-
 // Add toggle listener
-chrome.runtime.onMessage.addListener((msg, sender) => {
-  if (msg == 'toggle') {
+chrome.runtime.onMessage.addListener(({ message }, sender) => {
+  if (message === 'toggle') {
     toggle();
   }
 });
